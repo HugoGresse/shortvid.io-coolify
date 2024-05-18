441f9148ff1b64556935acd2d8e66a123aa86710
Hugo Gresse <hugo.gresse@gmail.com>
2024-05-18 15:27:31 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -20,11 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(
-	cors({
-		origin: [/.*.shortvid\.io$/, /shortvid-git-.*-lyonjs.vercel.app$/],
-		optionsSuccessStatus: 200,
-		methods: 'POST',
-	}),
+	cors(),
 );
 app.use(morgan('tiny'));
 
